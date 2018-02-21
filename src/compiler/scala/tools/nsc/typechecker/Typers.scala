aef22dd16d5d7f2ef69a5d8a8a96100e7ff2f0ed
jvican <jorgevc@fastmail.es>
2018-02-21 18:01:07 +0100

    Don't add original attachment for constant folded trees
    Scalac seems to run twice constant-folding in certain scenarios.
    Consider the following example:
    
    ```scala
    object A { final val x = 1 }
    
    object B {
      def main(args: Array[String]) = assert(args(0).toInt == A.x )
    }
    ```
    
    The tree `A.x` in `main` is constant folded because `x` is a constant,
    but for some reason the resulting tree `Literal(Constant(1))` get
    constant-folded again.
    
    The previous logic would add the original tree attachment on the
    constant-folded tree, and would destroy the previous annotation that
    gets copied by `treeCopy.Literal`.
    
    Therefore, to avoid this issue, we don't update the original attachment
    if there already exists one.
    
    In the future, it would be worthwhile to figure out why scalac tries to
    constant-fold twice the same tree.

--- src/compiler/scala/tools/nsc/typechecker/Typers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -995,7 +995,8 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
         if (sym != null && sym.isDeprecated)
           context.deprecationWarning(tree.pos, sym)
         // Keep the original tree in an annotation to avoid losing tree information for plugins
-        treeCopy.Literal(tree, value).updateAttachment(OriginalTreeAttachment(original))
+        if (tree.hasAttachment[OriginalTreeAttachment]) treeCopy.Literal(tree, value)
+        else treeCopy.Literal(tree, value).updateAttachment(OriginalTreeAttachment(tree))
       }
 
       // Ignore type errors raised in later phases that are due to mismatching types with existential skolems
