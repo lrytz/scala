929d01ef9d76b599d5681510e8338d2285b9f0c1
Eugene Yokota <eed3si9n@gmail.com>
2019-05-14 07:45:33 -0400

    apply header to Java files

--- src/compiler-bridge/xsbt/DottydocRunner.java
+++ src/compiler-bridge/xsbt/DottydocRunner.java
@@ -1,6 +1,15 @@
-/* sbt -- Simple Build Tool
- * Copyright 2008, 2009 Mark Harrah
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
  */
+
 package xsbt;
 
 import xsbti.Logger;
