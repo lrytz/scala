059cb67a8819212d40da6a595da62d5aca2dacda
Adriaan Moors <adriaan@lightbend.com>
2019-10-18 16:18:53 +0200

    omg we were calling this for java interfaces...

--- src/compiler/scala/tools/nsc/transform/Mixin.scala
+++ src/compiler/scala/tools/nsc/transform/Mixin.scala
@@ -401,7 +401,7 @@ abstract class Mixin extends Transform with ast.TreeDSL with AccessorSynthesis {
     // first complete the superclass with mixed in members
     addMixedinMembers(clazz.superClass, unit)
 
-    for (mc <- clazz.mixinClasses ; if mc.isTrait) {
+    for (mc <- clazz.mixinClasses ; if mc.isTrait && !mc.isJavaDefined) {
       // @SEAN: adding trait tracking so we don't have to recompile transitive closures
       unit.registerDependency(mc)
       publicizeTraitMethods(mc)
