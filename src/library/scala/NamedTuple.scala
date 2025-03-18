fc7027b469a5f2fcb39c851abd2e0acd1515fc43
Lukas Rytz <lukas.rytz@gmail.com>
2025-03-18 23:30:21 +0100

    implicitly convert from / to tuples

--- src/library/scala/NamedTuple.scala
+++ src/library/scala/NamedTuple.scala
@@ -1,5 +1,7 @@
 package scala
 
+import scala.language.implicitConversions
+
 object NamedTuple {
   sealed trait NamedTuple[N]
 
@@ -15,5 +17,13 @@ object NamedTuple {
       def apply[T1, T2](_1: T1, _2: T2) = new NamedTuple2[Nothing, T1, T2](_1, _2)
     }
     def apply[N <: (String, String)]: Apply2[N] = Apply2.asInstanceOf[Apply2[N]]
+
+    // Scala 3 also allows an unnamed tuple when a named is expected
+    implicit def fromTuple[N <: (String, String), T1, T2](t: (T1, T2)): NamedTuple2[N, T1, T2] =
+      new NamedTuple2[N, T1, T2](t._1, t._2)
+
+    // Scala 3 also implicitly converts to tuple
+    implicit def toTuple[N <: (String, String), T1, T2](n: NamedTuple2[N, T1, T2]): (T1, T2) =
+      n.toTuple
   }
 }
--- test/files/run/named-tuples.scala
+++ test/files/run/named-tuples.scala
@@ -1,16 +1,23 @@
 import NamedTuple._
 object Test extends App {
   def show(x: NamedTuple2[("name", "age"), String, Int]) = s"${x.name}: ${x.age}"
+  def wohs(x: (name: String, age: Int)) = show(x)
+
+  type P = (name: String, age: Int)
 
   val w1: NamedTuple2[("name", "age"), String, Int] = NamedTuple2("gandalf", 55000)
   val w2 = NamedTuple2[("name", "age")]("dumbledore", 115)
   val w3 = (name = "harry", age = 11)
   val w4: (name: String, age: Int) = w1
+  val w5: (name: String, age: Int) = ("snape", 31) // implicit fromTuple
+  val w6: P = w2.toTuple     // implicit fromTuple
+  val w7: (String, Int) = w6 // implicit toTuple
 
   assert(show(w1) == "gandalf: 55000")
   assert(show(w2) == "dumbledore: 115")
   assert(show(w3) == "harry: 11")
-  assert(show(w4) == show(w1))
+  assert(show(w4) == wohs(w1))
+  assert(show(w5) == "snape: 31")
 
   assert(w3.toString == "(harry, 11)")
 }
